c2625c176946d4218f63b7c164afb48db3f3cf35
Bozhidar Dimitrov <bojovidi@gmail.com>
2014-02-02 03:22:57 +0100

    Initial setup.
    Mod InitialMenu
    Mod Manifest
    Add README.txt (read it!)
    Delete initial_menu.xml

---
+++ GwBo/README.txt
@@ -0,0 +1,22 @@
+- GwBo testova aplikaciq -
+
+Instrukcii:
+
+1. Sled kato se podgotvi nov class(file) se dobavq v spisyka "classes" v class-a
+InitialMenu. Imeto trqbva da e napisano tochno. Ako se namira v pod-direktoriq
+se polzva ".", za da se opishe pytq do nego.
+2. Vsi4ki promeni se opisvat nakratko vyv "commit message". Neka se znae
+sys vseki update kakvi failove se dobavqt/iztrivat.
+
+Preporychitelno:
+1. Ako nqkoi se seti za pravilo, info i t.n., koeto da si zaslujava da se otbeleji -
+da go zapishe tuk (i, razbira se, da spomene za tova vyv commit message).
+2. Moje da napravim papka, v koqto da sybirame polezna informaciq i tutorialcheta,
+koito namirame prez dylgite i SAMOTNI noshti na razrushitelno programirane :)
+
+PS: InitialMenu.classes sym mu slojil 2 elementa da moje da ima neshto predi
+da sme go pochnali sys activity-ta. Kato se kliknat ne rabotqt. Shte gi iztriq,
+kato slojim pyrvite "istinski" bebcheta :)
+
+Info:
+EGit - http://www.vogella.com/tutorials/EclipseGit/article.html#usingegit_overview
--- GwBo/src/com/example/gwbo/InitialMenu.java
+++ GwBo/src/com/example/gwbo/InitialMenu.java
@@ -2,21 +2,39 @@ package com.example.gwbo;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class InitialMenu extends Activity {
+public class InitialMenu extends ListActivity {
+	
+	String[] classes = {"class1" , "class2"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.initial_menu);
+		
+		setListAdapter(new ArrayAdapter<String>(InitialMenu.this, android.R.layout.simple_list_item_1, classes));
 	}
-
+	
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.initial_menu, menu);
-		return true;
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+
+		String strClass = classes[position];
+
+		try {
+			Class clickedClass = Class.forName("com.probe.probe." + strClass);
+
+			Intent intent = new Intent(InitialMenu.this, clickedClass);
+			startActivity(intent);
+		}
+		catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
 	}
 
 }
