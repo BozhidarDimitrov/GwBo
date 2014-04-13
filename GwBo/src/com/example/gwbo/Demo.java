35d6eb69b53ddd6db428082de06c819304b8d785
Bozhidar Dimitrov <bojovidi@gmail.com>
2014-04-13 21:14:29 +0200

    bojo president 3

--- GwBo/src/com/example/gwbo/Demo.java
+++ GwBo/src/com/example/gwbo/Demo.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 
 public class Demo extends AbstractFragmentAcitivty implements OnClickListener {
 	
+	//jivko pederast
 	private Button sub;
 	private TextView tv;
 	private int counter;
